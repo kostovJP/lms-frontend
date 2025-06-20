3d77653a9c39f3b96726cdd49512efd6c62ff275
Kaustav <kaustavphukan44@gmail.com>
2025-06-20 23:13:30 +0530

    updated sidebar and homepage content

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,9 +1,10 @@
 const Sidebar = () => {
   return (
     <aside className="hidden md:block w-64 bg-gray-100 p-4">
-      <ul>
+      <ul className="text-blue-500">
         <li className="mb-2"><a href="#">Menu Item 1</a></li>
         <li className="mb-2"><a href="#">Menu Item 2</a></li>
+        <p className="text-lg font-semibold">This is the sidebar</p>
       </ul>
     </aside>
   );
